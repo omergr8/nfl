b3dbd28949c8b6e0e0a5101ed81de5cd03aa9f9e
omergr8 <umar.mukhtar4455@gmail.com>
2021-08-19 05:50:50 +0500

    fixed route on registartion

--- src/Pages/Common/RegistrationNavbar/RegistrationNavbar.js
+++ src/Pages/Common/RegistrationNavbar/RegistrationNavbar.js
@@ -1,17 +1,20 @@
 import classes from "./RegistrationNavbar.module.css";
 import logo from "../../../Assets/logo.svg";
 import Button from "@material-ui/core/Button";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 const RegistrationNavbar = () => {
   const history = useHistory();
 
   function handleClick() {
-    history.push("/home");
+    history.push("/");
   }
   return (
     <div>
       <div className={classes.navFlex}>
-        <img src={logo} width="100px" alt="logo" />
+        <Link to="/">
+          <img src={logo} width="100px" alt="logo" />
+        </Link>
+
         <Button className={classes.backButton} onClick={handleClick}>
           Back
         </Button>
